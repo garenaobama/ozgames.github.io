da32a9541a3c6a67e2eb36551c3453086643091a
garenaobama <64473652+garenaobama@users.noreply.github.com>
2021-02-20 14:23:56 +0700

    done
    20-2-2021

---
+++ README.md
@@ -0,0 +1,1 @@
+# ozgames.github.io
---
+++ ozgamesscript.js
@@ -0,0 +1,18 @@
+function clickevent() {
+   var x=document.getElementById("drop-down-task-bar");
+   if( x.style.display== "none") {
+      x.style.display="block";
+   }
+   else {
+      x.style.display="none";
+   }
+}
+function scroll_view(){
+   var x=document.getElementById("mps-iframe");
+   x.scrollIntoView();
+}
+function set_height(){
+   var the_height = document.getElementById('mps-iframe').contentDocument.document.body.scrollHeight;
+  //change the height of the iframe
+  document.getElementById('mps-iframe').height=the_height;
+}
